5e91d95038f8b4e1cb0f583255bf5bdba0bd209e
Liza Daly <liza@threepress.org>
2009-01-15 15:57:42 -0500

    The great renaming

--- templatetags/typogrify_tags.py
+++ templatetags/typogrify_tags.py
@@ -6,7 +6,7 @@ from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_unicode
 
-import smartypants as _smartypants
+import typogrify.smartypants as _smartypants
 
 register = template.Library()
 
