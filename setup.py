193c4a6c305096aa82438833690474dad7b7075e
Chris Drackett <chris@chrisdrackett.com>
2009-11-24 17:53:17 -0500

    adding setup.py (thank liz)

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='typogrify',
+    version='1.1',
+    description='Make type not look like crap (for django apps)',
+    author='Christian Metts, Chris Drackett',
+    author_email='chris@shelfworthy.com',
+    url='http://github.com/chrisdrackett/django-typogrify',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers :: Designers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
