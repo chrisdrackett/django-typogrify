68463ff6a4419831adccbc39eae0600edb5fd533
Chris Drackett <chris@chrisdrackett.com>
2011-01-08 14:14:03 -0800

    install requirements in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Christian Metts, Chris Drackett',
     author_email='chris@shelfworthy.com',
     url='http://github.com/chrisdrackett/django-typogrify',
+    install_requires=read_file('requirements.txt'),
     packages = [
         "typogrify",
         "typogrify.templatetags",
