ea4f558a1c2c81a7263fa0dac9c9051027a68829
Chris Drackett <chris@chrisdrackett.com>
2011-03-02 11:31:01 -0600

    bumping version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-typogrify',
-    version='1.2',
+    version='1.2.1',
     description='Make type not look like crap (for django apps)',
     author='Chris Drackett',
     author_email='chris@shelfworthy.com',
