2c087731ceca705ba51c4412632396dca0a633b4
Chris Drackett <chris@chrisdrackett.com>
2012-03-30 17:05:12 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.2'
+VERSION = '1.3'
 
 setup(
     name='django-typogrify',
     version=VERSION,
     description='Make type not look like crap (for django apps)',
     author='Chris Drackett',
-    author_email='chris@shelfworthy.com',
+    author_email='chris@tiltshiftstudio.com',
     url='http://github.com/chrisdrackett/django-typogrify',
     packages = [
         "typogrify",
